495f6af838596b371bfb557a6a2db292b777e355
vayers <violetlastname@gmail.com>
2023-10-26 09:51:25 -0400

    format things, closed pringwriters and scanners cause linter was mad

--- src/CardReaderIdExtractor.java
+++ src/CardReaderIdExtractor.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 
 class CardReaderIdExtractor {
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         if (args.length != 1) {
             System.out.println("You need to enter a file!");
             return;
@@ -13,31 +13,31 @@ class CardReaderIdExtractor {
         Scanner s;
         try {
             s = new Scanner(new FileReader(args[0]));
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Invalid file '" + args[0] + "'");
             return;
         }
 
         Scanner input = new Scanner(System.in);
         System.out.print("output file: ");
+        input.close();
+
         String output = input.nextLine();
 
-        PrintWriter pw; 
+        PrintWriter pw;
         try {
             pw = new PrintWriter(new FileWriter(output + ".txt"));
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("idk it broke");
             return;
         }
 
-        String id;
         String line;
         while (s.hasNextLine()) {
-            line = s.nextLine();  
+            line = s.nextLine();
             pw.println(line.substring(58, 67));
         }
         pw.flush();
+        pw.close();
     }
 }
